a2a7deddf74605636eac49ca7f90fbbbb598897d
VasileR <vlpure1@hotmail.com>
2025-07-14 18:19:58 +0100

    Create post functionality added

--- mobile/utils/api.ts
+++ mobile/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import { useAuth, useClerk } from "@clerk/clerk-expo";
 
-const API_BASE_URL = "https://pixly-ashy.vercel.app/api"
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const createApiClient = (getToken: () => Promise<string | null>): AxiosInstance => {
   const api = axios.create({ baseURL: API_BASE_URL });
