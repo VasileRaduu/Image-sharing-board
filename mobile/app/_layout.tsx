b769599467ea83b1ead0585052eb2c3130f36a4e
VasileR <vlpure1@hotmail.com>
2025-07-14 13:59:03 +0100

    user synched to db

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout() {
       <QueryClientProvider client={queryClient}>
 
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" />
         </Stack>
       </QueryClientProvider>
     </ClerkProvider>
